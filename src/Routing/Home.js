f4b482bbdcb6aefbc98bafec4241a6e5888d02fa
Nameera Pathan <66337671+dedsecwrench@users.noreply.github.com>
2022-04-10 00:31:18 +0530

    React4

--- src/Routing/Home.js
+++ src/Routing/Home.js
@@ -8,7 +8,6 @@ const Home = () => {
         <div className="card-body">
             <h5 className="card-title">Special title treatment</h5>
             <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
         </div>
         </div>
     </div>
