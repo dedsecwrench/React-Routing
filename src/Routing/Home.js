9da9532e42d9368167f7e4199c0d37815b35e67e
Nameera Pathan <66337671+dedsecwrench@users.noreply.github.com>
2022-04-10 00:33:24 +0530

    React4

--- src/Routing/Home.js
+++ src/Routing/Home.js
@@ -3,13 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-         <div className="card">
-        <h5 className="card-header">Home</h5>
-        <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        </div>
-        </div>
+          <h2>Home</h2>
     </div>
   )
 }
